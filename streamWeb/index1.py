2f21dde36aa4825cd3414e3fc0499fa2a6326236
wuhungchi <168252347+wuhungchi@users.noreply.github.com>
2024-06-15 02:43:26 +0000

    streamlit

---
+++ streamWeb/index1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("這是一個測試")
+st.header("Header")
+st.subheader("subHeader")
+st.caption("caption")
+st.divider()
+st.markdown("## Hello! **_World_**")
+st.markdown("###步驟1")
+st.markdown("步驟2")
+st.code('''
+        ''')
